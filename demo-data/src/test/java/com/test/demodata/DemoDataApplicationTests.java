52f9a86c45d31f06593adcd3691d8cb3233a004e
yk <1362616851@qq.com>
2019-07-24 14:42:55 +0800

    第一次提交

---
+++ demo-data/src/test/java/com/test/demodata/DemoDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.demodata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoDataApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ demo-view/src/main/java/com/test/demoview/DemoViewApplication.java
@@ -0,0 +1,13 @@
+package com.test.demoview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoViewApplication.class, args);
+    }
+
+}
